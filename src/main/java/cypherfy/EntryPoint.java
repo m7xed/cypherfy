d4582be29934801c3ddc0d0dd1fa57051df2de72
m7xed <mrfieldcs@outlook.com>
2025-04-22 17:57:05 +0100

    Updated EntryPoint to correctly display TitleScreen Version 0.2.0 Alpha

---
+++ src/main/java/cypherfy/EntryPoint.java
@@ -0,0 +1,34 @@
+package cypherfy;
+
+import gui.CustomTitleBar;
+import gui.TitleScreen;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class EntryPoint extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        TitleScreen titleScreen = new TitleScreen(stage);
+
+        stage.setScene(titleScreen.getTitleScene());
+        stage.setTitle("Cypherfy");
+        stage.setWidth(800);
+        stage.setHeight(600);
+
+        stage.iconifiedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                CustomTitleBar.restoreWindow(stage);
+            }
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
