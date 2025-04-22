f437dc0d077dd150ced2effc87aabed1f7f9c96f
m7xed <mrfieldcs@outlook.com>
2025-04-22 15:03:31 +0100

    Initial Commit, Version 0.1.0 Alpha

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module mountain.cypherfy {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cypherfy to javafx.fxml;
+    exports cypherfy;
+}
