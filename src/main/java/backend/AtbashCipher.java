627a5d02c6f630be4e53ff0bfcb9de327ef326cd
m7xed <mrfieldcs@outlook.com>
2025-04-22 23:07:12 +0100

    Working AtbashCipher, 75% coverage tested, Version 1.0.0

---
+++ src/main/java/backend/AtbashCipher.java
@@ -0,0 +1,16 @@
+package backend;
+
+public class AtbashCipher {
+    public String atbashCipher(Character[] data) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            int ascii = (int) data[i];
+            if (ascii >= 65 && ascii <= 90) {
+                result.append((char) ('Z' - (data[i] - 'A')));
+            } else if (ascii >= 97 && ascii <= 122) {
+                result.append((char) ('z' - (data[i] - 'a')));
+            }
+        }
+        return result.toString();
+    }
+}
