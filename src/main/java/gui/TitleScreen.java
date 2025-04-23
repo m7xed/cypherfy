55bef4d9b5fe6ec5687baa66c17959d4386c3487
m7xed <mrfieldcs@outlook.com>
2025-04-23 16:34:29 +0100

    Custom font working, Version 0.3.0

--- src/main/java/gui/TitleScreen.java
+++ src/main/java/gui/TitleScreen.java
@@ -2,10 +2,10 @@ package gui;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 
 import javafx.stage.Stage;
+
 
 public class TitleScreen {
     private final Scene titleScreen;
@@ -16,20 +16,17 @@ public class TitleScreen {
 
         // Access Layouts
         BorderPane root = (BorderPane) titleScreen.getRoot();
-        CustomTitleBar titleBar = (CustomTitleBar) root.getTop();
         VBox vBox = (VBox) root.getCenter();
 
-        Label title = new Label("Cypherfy");
-        title.getStyleClass().add("label");
+        Button title = new Button("Cypherfy");
+        title.setStyle("-fx-font-family: 'SDDystopianDemo'; -fx-font-size: 48;");
+        title.getStyleClass().add("title");
 
-        Button encryptMenu = new Button("Encrypt Message");
-        encryptMenu.getStyleClass().add("encrypt-button");
-
-        encryptMenu.setOnAction(event -> {
-            screenManager.switchScene(new MenuScreen(stage, screenManager, titleBar).getScene());
+        title.setOnAction(event -> {
+            screenManager.switchScene(new MenuScreen(stage, screenManager).getMenuScene());
         });
 
-        vBox.getChildren().addAll(title, encryptMenu);
+        vBox.getChildren().addAll(title);
         screenManager.applyStyleSheets(titleScreen);
     }
 
