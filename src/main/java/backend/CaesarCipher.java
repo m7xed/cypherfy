255b85cffc0105ba3bffe76dda61daeed1b5044e
m7xed <mrfieldcs@outlook.com>
2025-04-24 14:00:36 +0100

    Updated cipher algorithm to run single pass, Version 1.0.1

--- src/main/java/backend/CaesarCipher.java
+++ src/main/java/backend/CaesarCipher.java
@@ -1,14 +1,16 @@
 package backend;
 
 public class CaesarCipher {
-    public String caesarCipher(Character[] data, int key) {
+    private static final int KEY = 1;
+
+    public Character[] caesarCipher(Character[] data) {
         for (int j = 0; j < data.length; j++) {
             int ascii = (int) data[j];
 
             if (ascii >= 65 && ascii <= 90) {
-                ascii = (ascii + key);
+                ascii = (ascii + KEY);
             } else if(ascii >= 97 && ascii <= 122) {
-                ascii = (ascii + key);
+                ascii = (ascii + KEY);
             }
 
             if (ascii >= 91 && ascii <= 96) {
@@ -19,10 +21,9 @@ public class CaesarCipher {
 
             data[j] = (char) ascii;
             }
-        StringBuilder sb = new StringBuilder();
-        for (Character datum : data) {
-            sb.append(datum);
-        }
-        return sb.toString();
+        Character[] newData = new Character[data.length];
+        System.arraycopy(data, 0, newData, 0, data.length);
+
+        return newData;
     }
 }
